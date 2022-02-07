d2a5ff548e371b87cab5c7302ff912596354cb2e
Katerina Zhadko <katerinkaj@icloud.com>
2022-02-08 02:52:06 +0300

    feat: add video

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -2,13 +2,14 @@ import portfolio from './portfolio.js';
 import burgerMenu from './burger.js';
 import theme from './theme.js';
 import toChangeLanguage from './lang.js';
+import playVideo from './video.js';
 
 
 function changeClassActive(element, selectorName) {
     const allElements = document.querySelectorAll(`.${selectorName}`);
     allElements.forEach(element => element.classList.remove("active"));
     element.classList.add("active");
-};
+}
 
 export default changeClassActive;
 
@@ -16,7 +17,4 @@ portfolio();
 burgerMenu();
 toChangeLanguage();
 theme();
-
-
-
-
+playVideo();
